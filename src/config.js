2a41de8e73df1e9b8147e712073291d252cd58a8
Brett Fieber <bfieber@loanpal.com>
2020-08-07 14:58:12 -0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: 'us-west-2',
+    BUCKET: 'brett123-notes-app-uploads'
+  },
+  apiGateway: {
+    REGION: 'us-west-2',
+    URL: 'https://uia9d0h0qe.execute-api.us-west-2.amazonaws.com/prod'
+  },
+  cognito: {
+    REGION: 'us-west-2',
+    USER_POOL_ID: 'us-west-2_usz32Xlov',
+    APP_CLIENT_ID: '5lj795t5p4eqje5fkuv446omav',
+    IDENTITY_POOL_ID: 'us-west-2:afb9af37-d822-40ac-9f06-95fbf90e663e'
+  }
+}
