db2f98462083d60ab85e9d680e963d1c310e9e1c
Brett Fieber <bfieber@loanpal.com>
2020-08-10 13:41:07 -0700

    feat: redirect on authentication

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
+  STRIPE_KEY: 'pk_test_51H5exXCne1S3xympNKhPvKedWJSvMJZnj4CtX7zBUTdj6xhNp204gYaaUilgzaTKBEQEXVaZ4QuW7QU6B4pXP0xU00c1II8lYF',
   MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
     REGION: 'us-west-2',
